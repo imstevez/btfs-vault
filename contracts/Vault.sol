7030b696846f296292865d02954ce013c321895c
steve <stevzhang01@gmail.com>
2022-11-15 21:08:19 +0800

    perf: vault

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -8,8 +8,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/ERC1967/ERC1967UpgradeUpgradeable.sol";
-import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-
 
 
 /**
@@ -22,22 +20,31 @@ as a beneficiary, we should always take into account the possibility that a cheq
 */
 contract Vault is ERC1967UpgradeUpgradeable,UUPSUpgradeable{
   using SafeMath for uint;
-  using EnumerableSet for EnumerableSet.AddressSet;
 
   event ChequeCashed(
     address indexed beneficiary,
     address indexed recipient,
     address indexed caller,
-    address token,
     uint totalPayout,
     uint cumulativePayout,
     uint callerPayout
   );
-  event ChequeBounced(address token);
-  event VaultWithdraw(address token, address indexed from, uint amount);
-  event VaultDeposit(address token, address indexed from, uint amount);
-  event MultiTokensAdded(address[] tokens);
-  event MultiTokensRemoved(address[] tokens);
+  event ChequeBounced();
+  event VaultWithdraw(address indexed from, uint amount);
+  event VaultDeposit(address indexed from, uint amount);
+
+  event MultiTokenChequeCashed(
+    address token,
+    address indexed beneficiary,
+    address indexed recipient,
+    address indexed caller,
+    uint totalPayout,
+    uint cumulativePayout,
+    uint callerPayout
+  );
+  event MultiTokenChequeBounced(address indexed token);
+  event MultiTokenVaultWithdraw(address indexed token, address indexed from, uint amount);
+  event MultiTokenVaultDeposit(address indexed token, address indexed from, uint amount);
 
   struct EIP712Domain {
     string name;
@@ -49,13 +56,15 @@ contract Vault is ERC1967UpgradeUpgradeable,UUPSUpgradeable{
     "EIP712Domain(string name,string version,uint256 chainId)"
   );
   bytes32 public constant CHEQUE_TYPEHASH = keccak256(
-    "Cheque(address vault,address beneficiary,address token,uint256 cumulativePayout)"
+    "Cheque(address vault,address beneficiary,uint256 cumulativePayout)"
+  );
+  bytes32 public constant MULTI_TOKEN_CHEQUE_TYPEHASH = keccak256(
+    "Cheque(address token, address vault,address beneficiary,uint256 cumulativePayout)"
   );
 
 
 
   // the EIP712 domain this contract uses
-  //function domain() internal pure returns (EIP712Domain memory) {
   function domain() internal view returns (EIP712Domain memory) {
     uint256 chainId;
     assembly {
@@ -99,8 +108,6 @@ contract Vault is ERC1967UpgradeUpgradeable,UUPSUpgradeable{
   /* indicates wether a cheque bounced in the past */
   bool public bounced;
 
-  /* supported multi-tokens set */
-  EnumerableSet.AddressSet private _multiTokensSet;
   /* paid out for multi-tokens */
   mapping (address => mapping(address => uint)) public multiTokensPaidOut;
   /* total paid out for multi-tokens */
@@ -122,13 +129,13 @@ contract Vault is ERC1967UpgradeUpgradeable,UUPSUpgradeable{
     token = ERC20(_token);
   }
 
-  /// @return the token(wbtt) balance of the Vault
+  /// @return the balance of the Vault
   function totalbalance() public view returns(uint) {
     return token.balanceOf(address(this));
   }
 
   /// @return the specified token balance of the Vault
-  function totalBalanceOf(address _token) public view returns(uint) {
+  function totalbalanceOf(address _token) public view returns(uint) {
     return ERC20(_token).balanceOf(address(this));
   }
 
@@ -143,117 +150,145 @@ contract Vault is ERC1967UpgradeUpgradeable,UUPSUpgradeable{
   function _cashChequeInternal(
     address beneficiary,
     address recipient,
-    address _token,
     uint cumulativePayout,
     bytes memory issuerSig
   ) internal {
     /* The issuer must have given explicit approval to the cumulativePayout, either by being the caller or by signature*/
     if (msg.sender != issuer) {
-      require(issuer == recoverEIP712(chequeHash(address(this), beneficiary, _token, cumulativePayout), issuerSig),
+      require(issuer == recoverEIP712(chequeHash(address(this), beneficiary, cumulativePayout), issuerSig),
         "invalid issuer signature");
     }
 
-    if (_token == address(token)) {
-      require(cumulativePayout > paidOut[beneficiary], "Vault: cannot cash");
-      uint totalPayout = cumulativePayout.sub(paidOut[beneficiary]);
-      uint balance = totalBalanceOf(_token);
-      /* let the world know that the issuer has over-promised on outstanding cheques */
-      if (totalPayout > balance) {
-        bounced = true;
-        emit ChequeBounced(_token);
-      }
-      require(totalPayout <= balance, "Vault: insufficient fund");
-
-      /* increase the stored paidOut amount to avoid double payout */
-      paidOut[beneficiary] = paidOut[beneficiary].add(totalPayout);
-      totalPaidOut = totalPaidOut.add(totalPayout);
-
-      /* do the actual payment */
-      require(token.transfer(recipient, totalPayout), "transfer failed");
-      emit ChequeCashed(beneficiary, recipient, msg.sender, _token, totalPayout, cumulativePayout, 0);
-    } else {
-      require(cumulativePayout > multiTokensPaidOut[_token][beneficiary], "Vault: cannot cash");
-      uint totalPayout = cumulativePayout.sub(multiTokensPaidOut[_token][beneficiary]);
-      uint balance = totalBalanceOf(_token);
-      /* let the world know that the issuer has over-promised on outstanding cheques */
-      if (totalPayout > balance) {
-        multiTokensBounced[_token] = true;
-        emit ChequeBounced(_token);
-      }
-      require(totalPayout <= balance, "Vault: insufficient fund");
-
-      /* increase the stored paidOut amount to avoid double payout */
-      multiTokensPaidOut[_token][beneficiary] = multiTokensPaidOut[_token][beneficiary].add(totalPayout);
-      multiTokensTotalPaidOut[_token] = multiTokensTotalPaidOut[_token].add(totalPaidOut);
-
-      /* do the actual payment */
-      require(ERC20(_token).transfer(recipient, totalPayout), "transfer failed");
-      emit ChequeCashed(beneficiary, recipient, msg.sender, _token, totalPayout, cumulativePayout, 0);
+    require(cumulativePayout > paidOut[beneficiary], "Vault: cannot cash");
+    uint totalPayout = cumulativePayout.sub(paidOut[beneficiary]);
+    uint balance = totalbalance();
+    /* let the world know that the issuer has over-promised on outstanding cheques */
+    if (totalPayout > balance) {
+      bounced = true;
+      emit ChequeBounced();
     }
+    require(totalPayout <= balance, "Vault: insufficient fund");
+
+    /* increase the stored paidOut amount to avoid double payout */
+    paidOut[beneficiary] = paidOut[beneficiary].add(totalPayout);
+    totalPaidOut = totalPaidOut.add(totalPayout);
+
+    /* do the actual payment */
+    require(token.transfer(recipient, totalPayout), "transfer failed");
+
+    emit ChequeCashed(beneficiary, recipient, msg.sender, totalPayout, cumulativePayout, 0);
+  }
+
+  function _multiTokenCashChequeInternal(
+    address _token,
+    address beneficiary,
+    address recipient,
+    uint cumulativePayout,
+    bytes memory issuerSig
+  ) internal {
+    if (_token == address(token)) {
+      _cashChequeInternal(beneficiary, recipient, cumulativePayout, issuerSig);
+      return;
+    }
+
+    /* The issuer must have given explicit approval to the cumulativePayout, either by being the caller or by signature*/
+    if (msg.sender != issuer) {
+      require(issuer == recoverEIP712(multiTokenChequeHash(_token, address(this), beneficiary, cumulativePayout), issuerSig),
+        "invalid issuer signature");
+    }
+
+    require(cumulativePayout > multiTokensPaidOut[_token][beneficiary], "Vault: cannot cash");
+    uint totalPayout = cumulativePayout.sub(multiTokensPaidOut[_token][beneficiary]);
+    uint balance = totalbalanceOf(_token);
+    /* let the world know that the issuer has over-promised on outstanding cheques */
+    if (totalPayout > balance) {
+      bounced = true;
+      emit MultiTokenChequeBounced(_token);
+    }
+    require(totalPayout <= balance, "Vault: insufficient fund");
+
+    /* increase the stored paidOut amount to avoid double payout */
+    multiTokensPaidOut[_token][beneficiary] = paidOut[beneficiary].add(totalPayout);
+    multiTokensTotalPaidOut[_token] = multiTokensTotalPaidOut[_token].add(totalPayout);
+
+    /* do the actual payment */
+    require(ERC20(_token).transfer(recipient, totalPayout), "transfer failed");
+
+    emit MultiTokenChequeCashed(_token, beneficiary, recipient, msg.sender, totalPayout, cumulativePayout, 0);
   }
 
   /**
   @notice cash a cheque as beneficiary
   @param recipient receives the differences between cumulativePayment and what was already paid-out to the beneficiary minus callerPayout
-  @param _token the specified cash token
   @param cumulativePayout amount requested to pay out
   @param issuerSig issuer must have given explicit approval on the cumulativePayout to the beneficiary
   */
-  function cashChequeBeneficiary(address recipient, address _token, uint cumulativePayout, bytes memory issuerSig) public {
-    _cashChequeInternal(msg.sender, recipient, _token, cumulativePayout, issuerSig);
+  function cashChequeBeneficiary(address recipient, uint cumulativePayout, bytes memory issuerSig) public {
+    _cashChequeInternal(msg.sender, recipient, cumulativePayout, issuerSig);
   }
 
-  function withdraw(address _token, uint amount) public {
+  function multiTokenCashChequeBeneficiary(address _token, address recipient, uint cumulativePayout, bytes memory issuerSig) public {
+    _multiTokenCashChequeInternal(_token, msg.sender, recipient, cumulativePayout, issuerSig);
+  }
+
+  function withdraw(uint amount) public {
     /* only issuer can do this */
     require(msg.sender == issuer, "not issuer");
     /* ensure we don't take anything from the hard deposit */
-    require(amount <= totalBalanceOf(_token), "total balance not sufficient");
+    require(amount <= totalbalance(), "totalbalance not sufficient");
+    require(token.transfer(issuer, amount), "transfer failed");
+    emit VaultWithdraw(issuer, amount);
+  }
+
+  function multiTokenWithdraw(address _token, uint amount) public {
+    if (_token == address(token)) {
+      withdraw(amount);
+      return;
+    }
+    /* only issuer can do this */
+    require(msg.sender == issuer, "not issuer");
+    /* ensure we don't take anything from the hard deposit */
+    require(amount <= totalbalanceOf(_token), "totalbalance not sufficient");
     require(ERC20(_token).transfer(issuer, amount), "transfer failed");
-    emit VaultWithdraw(_token, issuer, amount);
+    emit MultiTokenVaultWithdraw(_token, issuer, amount);
   }
 
   /*
   * deposit wbtt to address(this), befrore it, must approve to address(this)
   */
-  function deposit(address _token, uint amount) public {
-    require(ERC20(_token).transferFrom(msg.sender, address(this), amount), "deposit failed");
-    emit VaultDeposit(_token, msg.sender, amount);
+  function deposit(uint amount) public {
+    require(token.transferFrom(msg.sender, address(this), amount), "deposit failed");
+    emit VaultDeposit(msg.sender, amount);
   }
 
-  function chequeHash(address vault, address beneficiary, address _token, uint cumulativePayout)
+  function multiTokenDeposit(address _token, uint amount) public {
+    if (_token == address(token)) {
+      deposit(amount);
+      return;
+    }
+    require(ERC20(_token).transferFrom(msg.sender, address(this), amount), "deposit failed");
+    emit MultiTokenVaultDeposit(_token, msg.sender, amount);
+  }
+
+  function chequeHash(address vault, address beneficiary, uint cumulativePayout)
   internal pure returns (bytes32) {
     return keccak256(abi.encode(
         CHEQUE_TYPEHASH,
         vault,
         beneficiary,
-        _token,
         cumulativePayout
       ));
   }
 
-  function addMultiTokens(address[] calldata _tokens) external {
-    require(msg.sender == issuer, "not issuer");
-    for (uint256 i = 0; i < _tokens.length; i++) {
-      require(_tokens[i] != address(0), "invalid token");
-      _multiTokensSet.add(_tokens[i]);
-    }
-    emit MultiTokensAdded(_tokens);
-  }
-
-  function removeMultiTokens(address[] calldata _tokens) external {
-    require(msg.sender == issuer, "not issuer");
-    for (uint256 i = 0; i < _tokens.length; i++) {
-      _multiTokensSet.remove(_tokens[i]);
-    }
-    emit MultiTokensRemoved(_tokens);
-  }
-
-  function getMultiTokens() external view returns (address[] memory) {
-    return _multiTokensSet.values();
-  }
-
-  function getMultiToken(address _token) external view returns (bool) {
-    return _multiTokensSet.contains(_token);
+  function multiTokenChequeHash(address _token, address vault, address beneficiary, uint cumulativePayout)
+  internal pure returns (bytes32) {
+    return keccak256(abi.encode(
+        MULTI_TOKEN_CHEQUE_TYPEHASH,
+        _token,
+        vault,
+        beneficiary,
+        cumulativePayout
+      ));
   }
 
   function _authorizeUpgrade(address) internal  view override {
